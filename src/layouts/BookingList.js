c6ed716d243b7224685dd9ca71025f05ac644da4
ajithkr12 <ajithkr.akr012kaippayil@gmail.com>
2023-01-02 13:52:58 +0530

    add Booking list....

--- src/layouts/BookingList.js
+++ src/layouts/BookingList.js
@@ -7,111 +7,43 @@ import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
-import TablePagination from '@mui/material/TablePagination';
 import TableRow from '@mui/material/TableRow';
-import TableSortLabel from '@mui/material/TableSortLabel';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Paper from '@mui/material/Paper';
 import Checkbox from '@mui/material/Checkbox';
 import IconButton from '@mui/material/IconButton';
-import Tooltip from '@mui/material/Tooltip';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Switch from '@mui/material/Switch';
 import DeleteIcon from '@mui/icons-material/Delete';
-import FilterListIcon from '@mui/icons-material/FilterList';
-import { visuallyHidden } from '@mui/utils';
 
-function createData(name, calories, fat, carbs, protein) {
+import Dots from '../Components/Dots';
+
+function createData(uhid, patientName, surgeonName, surgeryName, pac,admit,ready) {
   return {
-    name,
-    calories,
-    fat,
-    carbs,
-    protein,
+    uhid, 
+    patientName, 
+    surgeonName, 
+    surgeryName, 
+    pac,
+    admit,
+    ready
   };
 }
 
 const rows = [
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Donut', 452, 25.0, 51, 4.9),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-  createData('Honeycomb', 408, 3.2, 87, 6.5),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Jelly Bean', 375, 0.0, 94, 0.0),
-  createData('KitKat', 518, 26.0, 65, 7.0),
-  createData('Lollipop', 392, 0.2, 98, 0.0),
-  createData('Marshmallow', 318, 0, 81, 2.0),
-  createData('Nougat', 360, 19.0, 9, 37.0),
-  createData('Oreo', 437, 18.0, 63, 4.0),
+  createData('ABGTH1245687', "Ravi Shangar", "Alex", "Xyz abc", "Y","Y","Y"),
+  createData('ABGTH1245687', "Ravi Shangar", "Alex", "Xyz abc", "Y","Y","Y"),
+  createData('ABGTH1245687', "Ravi Shangar", "Alex", "Xyz abc", "Y","Y","Y"),
+  createData('ABGTH1245687', "Ravi Shangar", "Alex", "Xyz abc", "Y","Y","Y"),
+  createData('ABGTH1245687', "Ravi Shangar", "Alex", "Xyz abc", "Y","Y","Y"),
+  createData('ABGTH1245687', "Ravi Shangar", "Alex", "Xyz abc", "Y","Y","Y"),
+  createData('ABGTH1245687', "Ravi Shangar", "Alex", "Xyz abc", "Y","Y","Y"),
+  createData('ABGTH1245687', "Ravi Shangar", "Alex", "Xyz abc", "Y","Y","Y"),
+  createData('ABGTH1245687', "Ravi Shangar", "Alex", "Xyz abc", "Y","Y","Y"),
+  createData('ABGTH1245687', "Ravi Shangar", "Alex", "Xyz abc", "Y","Y","Y"),
+  createData('ABGTH1245687', "Ravi Shangar", "Alex", "Xyz abc", "Y","Y","Y"),
+  createData('ABGTH1245687', "Ravi Shangar", "Alex", "Xyz abc", "Y","Y","Y"),
+  createData('ABGTH1245687', "Ravi Shangar", "Alex", "Xyz abc", "Y","Y","Y"),
 ];
-
-// function descendingComparator(a, b, orderBy) {
-//   if (b[orderBy] < a[orderBy]) {
-//     return -1;
-//   }
-//   if (b[orderBy] > a[orderBy]) {
-//     return 1;
-//   }
-//   return 0;
-// }
-
-// function getComparator(order, orderBy) {
-//   return order === 'desc'
-//     ? (a, b) => descendingComparator(a, b, orderBy)
-//     : (a, b) => -descendingComparator(a, b, orderBy);
-// }
-
-// Since 2020 all major browsers ensure sort stability with Array.prototype.sort().
-// stableSort() brings sort stability to non-modern browsers (notably IE11). If you
-// only support modern browsers you can replace stableSort(exampleArray, exampleComparator)
-// with exampleArray.slice().sort(exampleComparator)
-// function stableSort(array, comparator) {
-//   const stabilizedThis = array.map((el, index) => [el, index]);
-//   stabilizedThis.sort((a, b) => {
-//     const order = comparator(a[0], b[0]);
-//     if (order !== 0) {
-//       return order;
-//     }
-//     return a[1] - b[1];
-//   });
-//   return stabilizedThis.map((el) => el[0]);
-// }
-
-// const headCells = [
-//   {
-//     id: 'name',
-//     numeric: false,
-//     disablePadding: true,
-//     label: 'Dessert (100g serving)',
-//   },
-//   {
-//     id: 'calories',
-//     numeric: true,
-//     disablePadding: false,
-//     label: 'Calories',
-//   },
-//   {
-//     id: 'fat',
-//     numeric: true,
-//     disablePadding: false,
-//     label: 'Fat (g)',
-//   },
-//   {
-//     id: 'carbs',
-//     numeric: true,
-//     disablePadding: false,
-//     label: 'Carbs (g)',
-//   },
-//   {
-//     id: 'protein',
-//     numeric: true,
-//     disablePadding: false,
-//     label: 'Protein (g)',
-//   },
-// ];
 
 function EnhancedTableHead(props) {
   const { onSelectAllClick, order, orderBy, numSelected, rowCount, onRequestSort } =
@@ -190,23 +122,10 @@ function EnhancedTableToolbar(props) {
           id="tableTitle"
           component="div"
         >
-          Nutrition
+          Lists
         </Typography>
       )}
 
-      {numSelected > 0 ? (
-        <Tooltip title="Delete">
-          <IconButton>
-            <DeleteIcon />
-          </IconButton>
-        </Tooltip>
-      ) : (
-        <Tooltip title="Filter list">
-          <IconButton>
-            <FilterListIcon />
-          </IconButton>
-        </Tooltip>
-      )}
     </Toolbar>
   );
 }
@@ -306,14 +225,14 @@ export default function EnhancedTable() {
                         padding="none"
                         align="center"
                       >
-                        {row.name}
+                        {row.uhid}
                       </TableCell>
-                      <TableCell align="center">{row.calories}</TableCell>
-                      <TableCell align="center">{row.fat}</TableCell>
-                      <TableCell align="center">{row.carbs}</TableCell>
-                      <TableCell align="center">{row.protein}</TableCell>
-                      <TableCell align="center">{row.carbs}</TableCell>
-                      <TableCell align="center">{row.protein}</TableCell>
+                      <TableCell align="center">{row.patientName}</TableCell>
+                      <TableCell align="center">{row.surgeonName}</TableCell>
+                      <TableCell align="center">{row.surgeryName}</TableCell>
+                      <TableCell align="center"><Dots/></TableCell>
+                      <TableCell align="center"><Dots/></TableCell>
+                      <TableCell align="center"><Dots/></TableCell>
                     </TableRow>
                   );
                 })}
